31880e848f6af6499a90b1f1a043c67e69fb09fb
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-11-05 22:10:00 -0700

    brand router,mw,& model

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const brandRouter = require('./brands/brand-router');
 
+router.use('/brands', brandRouter);
 
-module.exports=router;
+module.exports = router;
---
+++ api/brands/brand-router.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const Brand = require('./brand-model');
+const { validateBrandExistsById, validateBrandNameUnique } = require('./brand-middleware');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const brands = await Brand.findAll();
+    res.status(200).json(brands);
+  } catch(err) {
+    next(err);
+  }
+});
+
+router.get('/:brand_id', validateBrandExistsById, (req, res) => {
+  res.status(201).json(req.brand);
+});
+
+router.post('/', validateBrandNameUnique, async (req, res, next) => {
+  try {
+    const brand = await Brand.create(req.body);
+    res.status(201).json(brand);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:brand_id', validateBrandExistsById, validateBrandNameUnique, async (req, res, next) => {
+  try {
+    const brand = await Brand.updateById(req.body, req.brand.brand_id);
+    res.status(200).json(brand);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:brand_id', validateBrandExistsById, async (req, res, next) => {
+  try {
+    const deletedBrand = await Brand.deleteById(req.brand.brand_id);
+    res.status(200).json({
+      brand_id: deletedBrand.brand_id
+    });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
+});
+
+module.exports = router;
